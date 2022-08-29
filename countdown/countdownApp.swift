fb626fda553eb4145f2505d202b1d7eff5b6d303
lior mada <liorm2006@gmail.com>
2022-08-29 15:03:12 +0300

    Initial Commit

---
+++ countdown/countdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  countdownApp.swift
+//  countdown
+//
+//  Created by lior madav on 29/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct countdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
